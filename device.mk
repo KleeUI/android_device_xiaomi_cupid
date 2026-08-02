119dc64cbdfd2521483742204545dfbd649b0819
herobrinejjjj <herobrinejjj@gmail.com>
2026-08-02 10:46:03 +0000

    cupid: Fix fstab availability during early boot
    Install the platform fstab in vendor and recovery in addition to the vendor ramdisk so second-stage init and vold can discover it. Use the stable metadata by-name alias exposed before bootdevice symlinks are available.
    
    Change-Id: I2593a6a39385a46e84d00b37ade38c7cd4bddedd

--- device.mk
+++ device.mk
@@ -65,6 +65,8 @@ BOARD_SHIPPING_API_LEVEL := 31
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     device/qcom/taro/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
     device/qcom/taro/ueventd-odm.rc:$(TARGET_COPY_OUT_ODM)/etc/ueventd.rc
 
