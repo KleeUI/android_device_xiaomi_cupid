0633d50a66d42c1e28b762d0ba1832a06abaac95
herobrinejjjj <herobrinejjj@gmail.com>
2026-08-02 11:05:11 +0000

    cupid: Restore normal-boot security service startup
    Ship device-owned normal-boot init definitions for qseecomd and the QTI KeyMint service ahead of recovery-derived vendor copies. Restore the listener notification socket and drmrpc credentials required by the secure-world service chain.
    
    Change-Id: I1880d66e08fe2b00309738034282fe7c51438912

--- device.mk
+++ device.mk
@@ -67,6 +67,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
     $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
     $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
+    $(DEVICE_PATH)/rootdir/etc/init/android.hardware.security.keymint-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.security.keymint-service-qti.rc \
+    $(DEVICE_PATH)/rootdir/etc/init/qseecomd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/qseecomd.rc \
     device/qcom/taro/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
     device/qcom/taro/ueventd-odm.rc:$(TARGET_COPY_OUT_ODM)/etc/ueventd.rc
 
